ae46ceb322961cd75c4684c7c9dff83662c528d3
Halksel <admin@adminnoMacBook-ea-2.local>
2018-04-27 22:09:18 +0900

    imas-data get

--- src/ch2/open-gbf-link.py
+++ src/ch2/open-gbf-link.py
@@ -1,19 +1,34 @@
 from lxml import html
-import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
 
-page = requests.get(
-    "http://gbf-wiki.com/index.php?%BF%CD%CA%AA%C9%BE%B2%C1SSR")
+with open("imas.html", "r") as f:
+    soup = f.read()
 
-page.raise_for_status()
+root = html.fromstring(soup)
 
-root = html.fromstring(page.text)
-# /html/body/table/tbody/tr/td[2]/div[2]/ul[1]/li[1]/a
+count = 183  # imas の人数
 
-xpath = '/html/body/table/tbody/tr/td[2]/div[2]/ul[1]/li[1]'
+result = pd.DataFrame(data=None, index=None, columns=['b', 'w', 'h'])
 
+for i in range(count):
+    name = root.xpath(
+        '/html/body/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]/div[5]/table/tbody/tr['
+        + str(i + 1) + ']/td[1]/a')[0]
+    b = root.xpath(
+        '/html/body/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]/div[5]/table/tbody/tr['
+        + str(i + 1) + ']/td[2]')[0].text
+    w = root.xpath(
+        '/html/body/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]/div[5]/table/tbody/tr['
+        + str(i + 1) + ']/td[3]')[0].text
+    h = root.xpath(
+        '/html/body/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]/div[5]/table/tbody/tr['
+        + str(i + 1) + ']/td[4]')[0].text
+    name = re.sub(r'\s', '', name.text_content())
+    b = re.sub(r'\s', '', b)
+    w = re.sub(r'\s', '', w)
+    h = re.sub(r'\s', '', h)
+    result.loc[name] = [b, w, h]
 
-# uls = root.xpath(xpath)
-#
-# print(xpath)
-# for ul in uls:
-#     print(ul.text_content())
+result.to_csv("imas.csv",encoding="shift_jis")
