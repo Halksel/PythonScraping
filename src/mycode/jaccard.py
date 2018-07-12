0fc0ea33347f1adf98e1523879bdcd4fc99390ad
Halksel <whentheycry0708@gmail.com>
2018-07-12 15:42:59 +0900

    estimate-function is too slow

--- src/mycode/jaccard.py
+++ src/mycode/jaccard.py
@@ -4,6 +4,7 @@ from collections import Counter, Mapping
 from math import log
 import hashlib
 import itertools
+import timeit
 
 T = TypeVar('T')
 
@@ -17,10 +18,9 @@ def jaccard(a: Set[T], b: Set[T]) -> float:
 def min_hash_sketch(a: Set[T], b: Set[T], hashs) -> float:
     count: int = 0
     k = len(hashs)
-    print("calculate " + str(k) + " min-hash")
     for f in hashs:
-        fa = min(map(lambda x: f(x), a))
-        fb = min(map(lambda x: f(x), b))
+        fa = min(map(f, a))
+        fb = min(map(f, b))
         if fa == fb:
             count += 1
 
@@ -32,10 +32,10 @@ def odd_sketch(a: Set[T], hashs, n) -> List[int]:
     hashFunction = create_hash((hashlib.sha256, 'afde'))
     S = []
     for f in hashs:
-        S.append(min(map(lambda x: f(x), a)))
+        S.append(min(map(f, a)))
     for x in S:
         hx = hashFunction(x)
-        s[hx % n] = ~s[hx % n]
+        s[hx % n] = 1 if s[hx % n] == 0 else 0
     return s
 
 
@@ -59,19 +59,30 @@ def create_hash(t):
     return func
 
 
-# a: Set[int] = set(random.randint(0, 100, 100))
-# b: Set[int] = set(random.randint(50, 150, 100))
-a = set(["さかい", "水炊き", "焼肉", "ゆうじ", "ハツ", "鶏ガラ", "登録", "雑炊"])
-b = set(["さかい", "水炊き", "焼肉", "ゆうじ", "ハツ", "鶏ガラ", "登録", "テール"])
-c = set(["さかい", "水炊き", "焼肉", "ゆうじ", "ハツ", "鶏ガラ", "登録", "テール", "雑炊"])
+a: Set[int] = set(random.randint(0, 10000, 10000))
+b: Set[int] = set(random.randint(0, 10000, 10000))
 
 print(a)
 print(b)
 
 fs = [hashlib.md5, hashlib.sha256, hashlib.sha224]
-shifts = ['', 'a', 'aa', 'aaa', 'fauif', 'sfhs', 'sghei', 'dfiuew', 'ddf', 'fefef', 's', 'sf', 'hg']
+shifts = [
+    '', 'a', 'aa', 'aaa', 'fauif', 'sfhs', 'sghei', 'dfiuew', 'ddf', 'fefef',
+    's', 'sf', 'hg', 'dfsd', 'fefe', 'vegewg', 'aegwef', '123d'
+]
 hashs = list(map(create_hash, itertools.product(fs, shifts)))
 hashFunction = create_hash((hashlib.sha256, 'afde'))
+
+# time measure
+# loop = 1
+# result = timeit.timeit('jaccard(a, b)', globals=globals(), number=loop)
+# print(result / loop)
+# result = timeit.timeit(
+#     'min_hash_sketch(a, b, hashs)', globals=globals(), number=loop)
+# print(result / loop)
+# result = timeit.timeit(
+#     'odd_sketch_hash(a, b, hashs, 1007)', globals=globals(), number=loop)
+# print(result / loop)
 
 print(jaccard(a, b))
 print(min_hash_sketch(a, b, hashs))
